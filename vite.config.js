927daa590480c1b1779bdc65931ca9cb38ea6da7
ThinhNgo767 <thinhngo767@gmail.com>
2026-07-02 20:58:26 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+import { cloudflare } from "@cloudflare/vite-plugin";
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cloudflare()],
   server: {
     // Proxy /api/* → server Node.js khi dev (tránh CORS)
     // proxy: {
